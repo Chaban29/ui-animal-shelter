cc9c8cb171f1c4b3d3384b2cb4dbcee6c588e620
Roman Chaban <roman.chaban1001@gmail.com>
2023-12-23 20:34:18 +0200

    Add About

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import './styles/main.scss';
 
 const App = () => {
   return (
